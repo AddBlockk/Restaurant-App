e7548bf6a39ba40718efb65360f4f5757615ebf0
Dmitry <09.09.2005@inbox.ru>
2023-09-29 05:50:50 +0400

    fix map controls

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -22,9 +22,9 @@ export default function Maps() {
           <Map defaultState={defaultState} width="100%" height="50vh">
             <Placemark geometry={[46.355489228569915, 48.07389460345459]} />
             <FullscreenControl options={{ float: "left" }} />
-            <ZoomControl options={{ float: "left" }} />
+            <ZoomControl />
             <GeolocationControl options={{ float: "right" }} />
-            <TrafficControl options={{ float: "right" }} />
+            <TrafficControl />
           </Map>
         </YMaps>
       </div>
