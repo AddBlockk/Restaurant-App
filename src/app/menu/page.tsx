75e8acb5a33547766eb1722250f5acf12640e5b2
Boltach228 <121123133+AddBlockk@users.noreply.github.com>
2024-02-27 16:55:35 +0400

    add redux toolkit and count for eats

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -6,34 +6,32 @@ import Skeleton from "react-loading-skeleton";
 
 const MenuPage = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] items-center">
-        {menu.map((category) => (
-          <Link
-            href={`/menu/${category.slug}`}
-            key={category.id}
-            className="relative bg-cover p-8 md:h-1/2"
-            style={{ backgroundImage: `url(${category.img})` }}>
-            <div
-              className={`text-${category.color} flex flex-col justify-between h-full w-[50%]`}>
-              <div>
-                <h1 className="uppercase font-bold text-3xl">
-                  {category.title}
-                </h1>
-                <p className="text-sm my-8">{category.desc}</p>
-              </div>
-              <button
-                className={`text-${
-                  category.color === "white" ? "black" : "black"
-                } py-2 px-4 rounded-md absolute bottom-4`}>
-                Подробнее
-              </button>
+    // <Suspense fallback={<Loading />}>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] items-center">
+      {menu.map((category) => (
+        <Link
+          href={`/menu/${category.slug}`}
+          key={category.id}
+          className="relative bg-cover p-8 md:h-1/2"
+          style={{ backgroundImage: `url(${category.img})` }}>
+          <div
+            className={`text-${category.color} flex flex-col justify-between h-full w-[50%]`}>
+            <div>
+              <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
+              <p className="text-sm my-8">{category.desc}</p>
             </div>
-          </Link>
-        ))}
-        <Skeleton />
-      </div>
-    </Suspense>
+            <button
+              className={`text-${
+                category.color === "white" ? "black" : "black"
+              } py-2 px-4 rounded-md absolute bottom-4`}>
+              Подробнее
+            </button>
+          </div>
+        </Link>
+      ))}
+      <Skeleton />
+    </div>
+    // </Suspense>
   );
 };
 
