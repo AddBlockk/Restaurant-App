44e328c6f5f469696a09bdef791ecd07a61d2a5b
Boltach228 <121123133+AddBlockk@users.noreply.github.com>
2024-02-28 05:34:21 +0400

    add redux toolkit and count for eats

--- src/app/redux/features/counter/counterSlice.ts
+++ src/app/redux/features/counter/counterSlice.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "@/app/redux/features/counter/counterSlice";
 
