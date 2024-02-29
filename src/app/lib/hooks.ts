8d892cb79b890b27454f461d4b092f4cb95c7cba
Boltach228 <121123133+AddBlockk@users.noreply.github.com>
2024-02-29 13:10:02 +0400

    delete custom hooks

--- src/app/lib/hooks.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useDispatch, useSelector, useStore } from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch, AppStore } from "./store";
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppStore: () => AppStore = useStore;
