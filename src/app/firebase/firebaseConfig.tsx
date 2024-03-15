845f6523324db63594c844e5f4dc5b12243401e8
Boltach228 <121123133+AddBlockk@users.noreply.github.com>
2024-03-16 01:17:03 +0400

    test firebase without dotenv

--- src/app/firebase/firebaseConfig.tsx
+++ src/app/firebase/firebaseConfig.tsx
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCJCjgrlQ93mYQ6Em5WGioeEd6bTjpMW2M",
+  authDomain: "restaurant-app-boltach.firebaseapp.com",
+  databaseURL:
+    "https://restaurant-app-boltach-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "restaurant-app-boltach",
+  storageBucket: "restaurant-app-boltach.appspot.com",
+  messagingSenderId: "88194177268",
+  appId: "1:88194177268:web:d260725f73d49fa0fbe6a1",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
